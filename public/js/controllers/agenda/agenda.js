aa782b5fc6f12aee342ad71a16311319bdaabfe4
Alex <alexandre.rosepizant@orange.com>
2014-06-24 20:47:56 +0200

    Update agenda module

--- public/js/controllers/agenda/agenda.js
+++ public/js/controllers/agenda/agenda.js
@@ -121,14 +121,340 @@ angular.module('mean.agenda').controller('agendaController', ['$scope', '$routeP
     $scope.eventSources = [$scope.events];
     $scope.changeLang('french');
 
-    $scope.map = {
-        center: {
-            latitude: 45,
-            longitude: -73
+    $scope.map = {control: {},
+      version: "uknown",
+      heatLayerCallback: function (layer) {
+        //set the heat layers backend data
+        var mockHeatLayer = new MockHeatLayer(layer);
+      },
+      showTraffic: true,
+      showBicycling: true,
+      showWeather: false,
+      showHeat: false,
+      center: {
+        latitude: 45.188529000000000000,
+        longitude: 5.724523999999974000
+      },
+      options: {
+        streetViewControl: true,
+        panControl: true,
+        maxZoom: 20,
+        minZoom: 3
+      },
+      zoom: 8,
+      dragging: false,
+      bounds: {},
+      markers: [
+        {
+          id: 1,
+          latitude: 45,
+          longitude: -74,
+          showWindow: false,
+          title: 'Marker 2'
         },
-        zoom: 8,
-        events: {
+        {
+          id: 2,
+          latitude: 15,
+          longitude: 30,
+          showWindow: false,
+          title: 'Marker 2'
+        },
+        {
+          id: 3,
+          icon: 'assets/images/plane.png',
+          latitude: 37,
+          longitude: -122,
+          showWindow: false,
+          title: 'Plane'
         }
+      ],
+      markers2: [
+        {
+          id: 1,
+          icon: 'assets/images/blue_marker.png',
+          latitude: 46,
+          longitude: -77,
+          showWindow: false,
+          title: '[46,-77]'
+        },
+        {
+          id: 2,
+          icon: 'assets/images/blue_marker.png',
+          latitude: 33,
+          longitude: -77,
+          showWindow: false,
+          title: '[33,-77]'
+        },
+        {
+          id: 3,
+          icon: 'assets/images/blue_marker.png',
+          latitude: 35,
+          longitude: -125,
+          showWindow: false,
+          title: '[35,-125]'
+        }
+      ],
+      mexiIdKey: 'mid',
+      mexiMarkers: [
+        {
+          mid: 1,
+          latitude: 29.302567,
+          longitude: -106.248779
+        },
+        {
+          mid: 2,
+          latitude: 30.369913,
+          longitude: -109.434814
+        },
+        {
+          mid: 3,
+          latitude: 26.739478,
+          longitude: -108.61084
+        }
+      ],
+      clickMarkers: [
+        {id: 1, "latitude": 50.948968, "longitude": 6.944781}
+        ,
+        {id: 2, "latitude": 50.94129, "longitude": 6.95817}
+        ,
+        {id: 3, "latitude": 50.9175, "longitude": 6.943611}
+      ],
+      dynamicMarkers: [],
+      randomMarkers: [],
+      doClusterRandomMarkers: true,
+      doUgly: true, //great name :)
+      clusterOptions: {title: 'Hi I am a Cluster!', gridSize: 60, ignoreHidden: true, minimumClusterSize: 2,
+        imageExtension: 'png', imagePath: 'assets/images/cluster', imageSizes: [72]},
+      clickedMarker: {
+        title: 'You clicked here',
+        latitude: null,
+        longitude: null
+      },
+      events: {
+        tilesloaded: function (map, eventName, originalEventArgs) {
+        },
+        click: function (mapModel, eventName, originalEventArgs) {
+          // 'this' is the directive's scope
+          console.log("user defined event: " + eventName, mapModel, originalEventArgs);
+
+          var e = originalEventArgs[0];
+
+          if (!$scope.map.clickedMarker) {
+            $scope.map.clickedMarker = {
+              title: 'You clicked here',
+              latitude: e.latLng.lat(),
+              longitude: e.latLng.lng()
+            };
+          }
+          else {
+            var marker = {
+              latitude: e.latLng.lat(),
+              longitude: e.latLng.lng()
+            };
+            $scope.map.clickedMarker = marker;
+          }
+          //scope apply required because this event handler is outside of the angular domain
+          $scope.$apply();
+        },
+        dragend: function () {
+          self = this;
+        }
+      },
+      infoWindow: {
+        coords: {
+          latitude: 36.270850,
+          longitude: -44.296875
+        },
+        options: {
+          disableAutoPan: true
+        },
+        show: false
+      },
+      infoWindowWithCustomClass: {
+        coords: {
+          latitude: 36.270850,
+          longitude: -44.296875
+        },
+        options: {
+          boxClass: 'custom-info-window'
+        },
+        show: true
+      },
+      templatedInfoWindow: {
+        coords: {
+          latitude: 48.654686,
+          longitude: -75.937500
+        },
+        options: {
+          disableAutoPan: true
+        },
+        show: true,
+        templateUrl: 'assets/templates/info.html',
+        templateParameter: {
+          message: 'passed in from the opener'
+        }
+      },
+      circles: [
+        {
+          id: 1,
+          center: {
+            latitude: 44,
+            longitude: -108
+          },
+          radius: 500000,
+          stroke: {
+            color: '#08B21F',
+            weight: 2,
+            opacity: 1
+          },
+          fill: {
+            color: '#08B21F',
+            opacity: 0.5
+          },
+          geodesic: true, // optional: defaults to false
+          draggable: true, // optional: defaults to false
+          clickable: true, // optional: defaults to true
+          editable: true, // optional: defaults to false
+          visible: true // optional: defaults to true
+        }
+      ],
+      polygons: [
+        {
+          id: 1,
+          path: [
+            {
+              latitude: 50,
+              longitude: -80
+            },
+            {
+              latitude: 30,
+              longitude: -120
+            },
+            {
+              latitude: 20,
+              longitude: -95
+            }
+          ],
+          stroke: {
+            color: '#6060FB',
+            weight: 3
+          },
+          editable: true,
+          draggable: true,
+          geodesic: false,
+          visible: true,
+          fill: {
+            color: '#ff0000',
+            opacity: 0.8
+          }
+        }
+      ],
+      polygons2: [
+        {
+          id: 1,
+          path: [
+            {
+              latitude: 60,
+              longitude: -80
+            },
+            {
+              latitude: 40,
+              longitude: -120
+            },
+            {
+              latitude: 45,
+              longitude: -95
+            }
+          ],
+          stroke: {
+            color: '#33CDDC',
+            weight: 3
+          },
+          editable: true,
+          draggable: true,
+          geodesic: false,
+          visible: true,
+          fill: {
+            color: '#33CCCC',
+            opacity: 0.8
+          }
+        }
+      ],
+      polylines: [
+        {
+          id: 1,
+          path: [
+            {
+              latitude: 45,
+              longitude: -74
+            },
+            {
+              latitude: 30,
+              longitude: -89
+            },
+            {
+              latitude: 37,
+              longitude: -122
+            },
+            {
+              latitude: 60,
+              longitude: -95
+            }
+          ],
+          stroke: {
+            color: '#6060FB',
+            weight: 3
+          },
+          editable: true,
+          draggable: true,
+          geodesic: true,
+          visible: true,
+          icons: [{
+            icon: { 
+              path: google.maps.SymbolPath.BACKWARD_OPEN_ARROW              
+            },
+            offset: '25px',
+            repeat: '50px'
+          }]
+        },
+        {
+          id: 2,
+          path: [
+            {
+              latitude: 47,
+              longitude: -74
+            },
+            {
+              latitude: 32,
+              longitude: -89
+            },
+            {
+              latitude: 39,
+              longitude: -122
+            },
+            {
+              latitude: 62,
+              longitude: -95
+            }
+          ],
+          stroke: {
+            color: '#6060FB',
+            weight: 3
+          },
+          editable: true,
+          draggable: true,
+          geodesic: true,
+          visible: true,
+          icons: [{
+            icon: { 
+              path: google.maps.SymbolPath.BACKWARD_OPEN_ARROW              
+            },
+            offset: '25px',
+            repeat: '50px'
+          }]
+        }
+      ]
+
     };
 
     $scope.map1 = {
