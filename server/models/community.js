33e69b51f107f814cf50df58a92b9627078a15db
alexrosepizant <alexandre.rosepizant@gmail.com>
2014-07-12 23:16:29 +0200

    Rename server folder

---
+++ server/models/community.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+
+/**
+ * Article Schema
+ */
+var CommunitySchema = new Schema({
+    name: String,
+    users: [{user: {
+        type: Schema.ObjectId,
+        ref: 'User'
+        }
+    }]
+});
+
+/**
+ * Statics
+ */
+CommunitySchema.statics.load = function(id, cb) {
+    this.findOne({
+        _id: id
+    }).populate('user', 'name username').exec(cb);
+};
+
+mongoose.model('Community', CommunitySchema);
