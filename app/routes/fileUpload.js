2343694e6d8d99144d03e65461d7b6cff014b35d
alexrosepizant <alexandre.rosepizant@gmail.com>
2014-07-12 23:14:00 +0200

    Rename app folder

--- app/routes/fileUpload.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Articles routes use articles controller
-var files = require('../controllers/files');
-//var authorization = require('./middlewares/authorization');
-
-module.exports = function(app) {
-    app.post('/upload/photo', files.uploadPhoto);
-};
