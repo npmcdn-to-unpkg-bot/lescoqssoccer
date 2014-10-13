006971b20b7edf9c62a1cf4f8088471abee52117
alexrosepizant <alexandre.rosepizant@gmail.com>
2014-10-14 00:24:35 +0200

    Improve articles module

--- src/js/articles/service.js
+++ src/js/articles/service.js
@@ -1,23 +1,23 @@
 'use strict';
 
 //Articles service used for Articles REST endpoint
-angular.module('mean.articles').factory('Articles', ['$resource',
-	function ($resource) {
-		return $resource('articles/:articleId', {
+angular.module( 'mean.articles' ).factory( 'Articles', [ '$resource',
+	function ( $resource ) {
+		return $resource( 'articles/:articleId', {
 			articleId: '@_id'
 		}, {
 			update: {
 				method: 'PUT'
 			}
-		});
+		} );
 	}
-]);
+] );
 
 /**
  * ArticleModel service
  **/
-angular.module('mean.articles').service('ArticlesCollection', ['Global', 'Articles',
-	function (Global, Articles) {
+angular.module( 'mean.articles' ).service( 'ArticlesCollection', [ 'Global', 'Articles',
+	function ( Global, Articles ) {
 
 		var global = Global;
 		var ArticlesCollection = {
@@ -31,110 +31,111 @@ angular.module('mean.articles').service('ArticlesCollection', ['Global', 'Articl
 
 			load: function () {
 
-				Articles.query(function (articles) {
+				Articles.query( function ( articles ) {
 
 					ArticlesCollection.all = [];
-					angular.forEach(articles, function (article) {
+					angular.forEach( articles, function ( article ) {
 
-						ArticlesCollection.all.push(article);
-						ArticlesCollection.all.sort(function (articleA, articleB) {
-							return new Date(articleB.created).getTime() - new Date(articleA.created).getTime();
-						});
+						ArticlesCollection.all.push( article );
+						ArticlesCollection.all.sort( function ( articleA, articleB ) {
+							return new Date( articleB.created ).getTime() - new Date( articleA.created ).getTime();
+						} );
 
 						ArticlesCollection.filtered = ArticlesCollection.all;
-						ArticlesCollection.readCount = ArticlesCollection.all.reduce(function (count, article) {
+						ArticlesCollection.readCount = ArticlesCollection.all.reduce( function ( count, article ) {
 							return article.read ? count : count;
-						}, 0);
-						ArticlesCollection.starredCount = ArticlesCollection.all.reduce(function (count, article) {
+						}, 0 );
+						ArticlesCollection.starredCount = ArticlesCollection.all.reduce( function ( count, article ) {
 							return article.starred ? count : count;
-						}, 0);
-						ArticlesCollection.selected = ArticlesCollection.selected ? ArticlesCollection.all.filter(function (article) {
+						}, 0 );
+						ArticlesCollection.selected = ArticlesCollection.selected ? ArticlesCollection.all.filter( function ( article ) {
 							return article.id == ArticlesCollection.selected.id;
-						})[0] : null;
-					});
-				});
+						} )[ 0 ] : null;
+					} );
+				} );
 			},
 
-			add: function (article, callback) {
+			findOne: function ( articleId ) {
 
-				var articleModel = new Articles(article);
-
-				articleModel.$save(function (response) {
-					ArticlesCollection.load();
-					callback.call();
-				});
+				return Articles.get( {
+					articleId: articleId
+				}, function ( article ) {
+					return article;
+				} ).$promise;
 			},
 
-			update: function (index, callback) {
+			add: function ( article ) {
 
-				if (index) {
-					ArticlesCollection.filtered[index].$update(function (response) {
-						ArticlesCollection.filtered[index] = response;
+				return Articles.save({}, article, function (data) {
+					return data;
+				}).$promise;
+			},
 
-						if (callback)
+			update: function ( index, callback ) {
+
+				if ( index ) {
+					ArticlesCollection.filtered[ index ].$update( function ( response ) {
+						ArticlesCollection.filtered[ index ] = response;
+
+						if ( callback )
 							callback.call();
-					});
+					} );
 				} else {
-					if (callback)
+					if ( callback )
 						callback.call();
 				}
 			},
 
-			remove: function (index, callback) {
-				if (index) {
-					$scope.filtered[index].$remove(function (response) {
-						if (callback)
-							callback.call();
-					});
-				} else {
-					if (callback)
-						callback.call();
-				}
+			remove: function ( article ) {
+
+				return Articles.delete( {}, article, function ( data ) {
+					return data;
+				} ).$promise;
 			},
 
 			prev: function () {
-				if (ArticlesCollection.hasPrev()) {
-					ArticlesCollection.selectArticle(ArticlesCollection.selected ? ArticlesCollection.selectedIdx - 1 : 0);
+				if ( ArticlesCollection.hasPrev() ) {
+					ArticlesCollection.selectArticle( ArticlesCollection.selected ? ArticlesCollection.selectedIdx - 1 : 0 );
 				}
 			},
 
 			next: function () {
-				if (ArticlesCollection.hasNext()) {
-					ArticlesCollection.selectArticle(ArticlesCollection.selected ? ArticlesCollection.selectedIdx + 1 : 0);
+				if ( ArticlesCollection.hasNext() ) {
+					ArticlesCollection.selectArticle( ArticlesCollection.selected ? ArticlesCollection.selectedIdx + 1 : 0 );
 				}
 			},
 
 			hasPrev: function () {
-				if (!ArticlesCollection.selected) {
+				if ( !ArticlesCollection.selected ) {
 					return true;
 				}
 				return ArticlesCollection.selectedIdx > 0;
 			},
 
 			hasNext: function () {
-				if (!ArticlesCollection.selected) {
+				if ( !ArticlesCollection.selected ) {
 					return true;
 				}
 				return ArticlesCollection.selectedIdx < ArticlesCollection.filtered.length - 1;
 			},
 
-			selectArticle: function (idx) {
+			selectArticle: function ( idx ) {
 
 				// Unselect previous selection.
-				if (ArticlesCollection.selected) {
+				if ( ArticlesCollection.selected ) {
 					ArticlesCollection.selected.selected = false;
 				}
 
-				ArticlesCollection.selected = ArticlesCollection.filtered[idx];
+				ArticlesCollection.selected = ArticlesCollection.filtered[ idx ];
 				ArticlesCollection.selectedIdx = idx;
 				ArticlesCollection.selected.selected = true;
 				ArticlesCollection.onCreation = ArticlesCollection.onEdition = false;
 			},
 
-			filterBy: function (key, value) {
-				ArticlesCollection.filtered = ArticlesCollection.all.filter(function (article) {
-					return article[key] === value;
-				});
+			filterBy: function ( key, value ) {
+				ArticlesCollection.filtered = ArticlesCollection.all.filter( function ( article ) {
+					return article[ key ] === value;
+				} );
 				ArticlesCollection.reindexSelectedItem();
 			},
 
@@ -146,4 +147,4 @@ angular.module('mean.articles').service('ArticlesCollection', ['Global', 'Articl
 
 		return ArticlesCollection;
 	}
-]);
+] );
