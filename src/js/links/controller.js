30e5c7ef6b91cdcfe85883db557428cd94ad51e5
arosepizant <alexandre.rosepizant@orange.com>
2014-11-03 18:59:24 +0100

    Improve css

--- src/js/links/controller.js
+++ src/js/links/controller.js
@@ -8,7 +8,21 @@ angular.module('mean.links').controller('LinkController', ['$scope', 'Global', '
 		$scope.LinksCollection = LinksCollection;
 		$scope.content;
 
-		$scope.LinksCollection.load();
+		//$scope.LinksCollection.load();
+		$scope.links = {
+			joan: {
+				author: "Jo",
+				adress: "http://objectifjapon.wordpress.com"
+			},
+			nico: {
+				author: "Nico",
+				adress: "http://deux-noix-nz.blogspot.fr"
+			},
+			momo:{
+				author: "Momo",
+				adress: "http://google.fr"
+			}
+		};
 
 		$scope.add = function () {
 
