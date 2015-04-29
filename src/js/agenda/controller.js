a960640654baed9fae7089ca8e163eebacc4076d
arosepizant <alexandre.rosepizant@orange.com>
2015-04-29 18:17:03 +0200

    AppStructure changes. WARN. Bugs

--- src/js/agenda/controller.js
+++ src/js/agenda/controller.js
@@ -3,6 +3,7 @@
 angular.module( 'mean.agenda' ).controller( 'createController', [ '$scope', '$routeParams', '$location', '$route', '$filter', 'Global', 'AgendaCollection', 'FileUploader', '$modal',
 	function ( $scope, $routeParams, $location, $route, $filter, Global, AgendaCollection, FileUploader, $modal) {
 
+		$scope.pageclass= "page-about";
 		$scope.agendaCollection = AgendaCollection;
 		$scope.view = $route.current.params.view;
 		$scope.eventTypes = eventTypes;
