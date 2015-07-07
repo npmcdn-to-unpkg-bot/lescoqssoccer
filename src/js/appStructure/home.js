8b1485acc55fc3bbf6ab5b012c7958d36d0f6f08
arosepizant <alexandre.rosepizant@orange.com>
2015-07-07 18:00:07 +0200

    Improve homepage

--- src/js/appStructure/home.js
+++ src/js/appStructure/home.js
@@ -3,9 +3,12 @@
 angular.module('mean.home').controller('HomeController', ['$scope', 'Global', '$location', 'TopMenu',
 	function($scope, Global, $location, TopMenu) {
 
+		console.warn(Global.user);
 		$scope.global = Global;
 		$scope.date = new Date();
 		$scope.dateFormat = "dd MMM yyyy";
+
+		$scope.caption = "Hola senor! Ça y est le site tant désiré (ou pas) est là, j'ai essayé de faire quelque chose assez simple pour que même les plus mauvais d'entre vous (et la concurrence est rude) s'en sorte, alors maintenant à vous pour le contenu, ça donne!!!!";
 
 		$scope.isCurrentPath = function(path) {
 			var cur_path = "#!" + $location.path().substr(0, path.length + 1);
@@ -14,11 +17,6 @@ angular.module('mean.home').controller('HomeController', ['$scope', 'Global', '$
 
 		$scope.closeMenu = function(){
 			TopMenu.close();
-		};
-
-		$scope.testTypingEffect = function() {
-			$scope.caption = "Hola senor! Ça y est le site tant désiré (ou pas) est là, j'ai essayé de faire quelque chose assez simple pour que même les plus mauvais d'entre vous (et la concurrence est rude) s'en sorte, alors maintenant à vous pour le contenu, ça donne!!!!";
-			$scope.type();
 		};
 
 		$scope.type = function() {
@@ -35,7 +33,12 @@ angular.module('mean.home').controller('HomeController', ['$scope', 'Global', '$
 		if($scope.isCurrentPath('home')){
 			setTimeout(function(){
 				$scope.captionLength = 0;
-				$scope.testTypingEffect();
+				$scope.type();
+			}, 700);
+		} else {
+			setTimeout(function(){
+				$('#caption').html($scope.caption);
+				$(".menu_cadonne").show();
 			}, 700);
 		}
 	}
