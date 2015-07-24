d94a62d06e005f52284b49a58295325199c2f199
arosepizant <alexandre.rosepizant@orange.com>
2015-07-24 11:54:43 +0200

    start to integrate template to app sidebar and homepage

--- src/js/appStructure/sidebar.js
+++ src/js/appStructure/sidebar.js
@@ -9,16 +9,16 @@ angular.module('mean.system').service('SideMenu',
 			show: true,
 			hasSearch: false,
 
-			setMenu: function(menu){
+			setMenu: function(menu) {
 				sideMenu.menu = menu;
 				sideMenu.show = true;
 			},
 
-			setSearchInput: function(hasSearch){
+			setSearchInput: function(hasSearch) {
 				sideMenu.hasSearch = hasSearch;
 			},
 
-			hide: function(){
+			hide: function() {
 				sideMenu.show = false;
 			}
 		};
@@ -58,5 +58,102 @@ angular.module('mean.system').controller('SidebarController', ['$scope', 'Global
 			})[0].callback();
 
 		};
+
+		setTimeout(function() {
+
+			/* WINDOW RESIZE FUNCTION */
+			jQuery(window).resize(function() {
+				jQuery("body").find('.page-title').show();
+				if (jQuery(window).width() > 480) {
+					jQuery('body').find('.leftcontainer').addClass('animated');
+				} else {
+					jQuery('body').find('.leftcontainer').removeClass('animated');
+				}
+			});
+
+			/* SIDEBAR ANIMATIONS */
+			jQuery('#menu-bar').find('.menu-bar-icon').click(function() {
+				"use strict";
+				jQuery('body').find('#menu-bar').addClass('menucolor');
+				jQuery('body').find('#menu-bg').fadeIn();
+				jQuery('body').find('.leftcontainer').show();
+				if (jQuery(window).width() > 480) {
+					jQuery('body').find('.leftcontainer').removeClass('animated fadeOutLeft');
+					jQuery('body').find('.leftcontainer').addClass('animated fadeInLeft');
+				} else {
+					jQuery('body').find('.leftcontainer').removeClass('animated');
+				}
+				jQuery(this).fadeOut();
+				setTimeout(function() {
+					jQuery('#menu-bar').find('.close-icon').fadeIn();
+				}, 400);
+			});
+
+			jQuery('#menu-bg').click(function() {
+				"use strict";
+				jQuery('body').find('#menu-bar').removeClass('menucolor');
+				jQuery('body').find('#menu-bg').fadeOut();
+				if (jQuery(window).width() > 480) {
+					jQuery('body').find('.leftcontainer').fadeOut();
+					jQuery('body').find('.leftcontainer').removeClass('animated fadeInLeft');
+					jQuery('body').find('.leftcontainer').addClass('animated fadeOutLeft');
+				} else {
+					jQuery('body').find('.leftcontainer').hide();
+					jQuery('body').find('.leftcontainer').removeClass('animated');
+				}
+				jQuery('#menu-bar').find('.close-icon').fadeOut();
+				setTimeout(function() {
+					jQuery('#menu-bar').find('.menu-bar-icon').fadeIn();
+				}, 400);
+			});
+
+			jQuery('#menu-bar').find('.close-icon').click(function() {
+				"use strict";
+				jQuery('body').find('#menu-bar').removeClass('menucolor');
+				jQuery('body').find('#menu-bg').fadeOut();
+				if (jQuery(window).width() > 480) {
+					jQuery('body').find('.leftcontainer').fadeOut();
+					jQuery('body').find('.leftcontainer').removeClass('animated fadeInLeft');
+					jQuery('body').find('.leftcontainer').addClass('animated fadeOutLeft');
+				} else {
+					jQuery('body').find('.leftcontainer').hide();
+					jQuery('body').find('.leftcontainer').removeClass('animated');
+				}
+				jQuery(this).fadeOut();
+				setTimeout(function() {
+					jQuery('#menu-bar').find('.menu-bar-icon').fadeIn();
+				}, 400);
+			});
+
+			jQuery('#mainmenu ul > li > a').click(function() {
+				"use strict";
+				var checkElement = jQuery(this).next();
+
+				if ((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+					checkElement.slideUp(300);
+					jQuery(this).removeClass("has-sub2");
+					jQuery(this).addClass("has-sub");
+					checkElement.removeClass("animated-fast fadeInLeft");
+					checkElement.addClass("animated-fast fadeOut");
+				}
+
+				if ((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+					jQuery('#mainmenu ul ul:visible').slideUp(300);
+					checkElement.slideDown(100);
+					jQuery('#mainmenu > ul > li:has(ul) > a').removeClass("has-sub2");
+					jQuery('#mainmenu > ul > li:has(ul) > a').addClass("has-sub");
+					jQuery(this).addClass("has-sub2");
+					checkElement.removeClass("animated-fast fadeOut");
+					checkElement.addClass("animated-fast fadeInLeft");
+				}
+
+				if (checkElement.is('ul')) {
+					return false;
+				} else {
+					return true;
+				}
+			});
+
+		}, 500);
 	}
 ]);
