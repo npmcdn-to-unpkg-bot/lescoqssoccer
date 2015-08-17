2c422aaad5239670667bfeb423eeb1dc8b95b91d
arosepizant <alexandre.rosepizant@orange.com>
2015-08-17 15:02:13 +0200

    Improve profile page

--- src/js/appStructure/global.js
+++ src/js/appStructure/global.js
@@ -3,13 +3,22 @@
 //Global service for global variables
 angular.module('mean.system').factory('Global', [
 
-	function () {
+	function() {
 		var _this = this;
 		_this._data = {
 			user: window.user,
-			authenticated: !! window.user
+			authenticated: !!window.user,
+			guid: guid
 		};
 
 		return _this._data;
 	}
 ]);
+
+var s4 = function() {
+	return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+};
+
+var guid = function() {
+	return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+};
