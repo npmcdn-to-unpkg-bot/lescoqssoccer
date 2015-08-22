396439ede067b4a12d0ce2f72bd4d8cb5d102e51
alexrosepizant <alexandre.rosepizant@gmail.com>
2015-08-23 01:53:29 +0200

    Add user detail view

---
+++ src/js/users/directive.js
@@ -0,0 +1,42 @@
+'use strict';
+
+angular.module('mean.users').directive("teamCarousel", function() {
+	return {
+		restrict: 'E',
+		transclude: false,
+		link: function(scope) {
+			scope.initCarousel = function(element) {
+				$(element).owlCarousel({
+					items: 1,
+					dots: false,
+					navText: ['', ''],
+					nav: true,
+					loop: true,
+					lazyLoad: true,
+					responsive: {
+						480: {
+							items: 1
+						},
+						640: {
+							items: 2
+						},
+						900: {
+							items: 3
+						}
+					}
+				});
+			};
+		}
+	};
+}).directive('teamCarouselItem', [function() {
+	return {
+		restrict: 'A',
+		transclude: false,
+		link: function(scope, element) {
+			// wait for the last item in the ng-repeat then call init
+			if (scope.$last) {
+				scope.initCarousel(element.parent());
+			}
+		}
+	};
+}]);
