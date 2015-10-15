4752f50364e5d0984553ba2f7b891ac3e3f93120
arosepizant <alexandre.rosepizant@orange.com>
2015-10-15 10:56:27 +0200

    Fix color change issues

--- src/js/appStructure/sidebar.js
+++ src/js/appStructure/sidebar.js
@@ -9,26 +9,33 @@ angular.module('mean.system').controller('SidebarController', ['$scope', 'Global
 		$scope.menu = [{
 			name: "ACCUEIL",
 			link: "#!/home",
-			id: 'home'
+			id: "home",
+			svg: "M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8V11h-8v10zm0-18v6h8V3h-8z"
 		}, {
 			name: "ARTICLES",
-			id: 'articles',
-			link: "#!/articles"
+			id: "articles",
+			link: "#!/articles",
+			svg:"M11.99 18.54l-7.37-5.73L3 14.07l9 7 9-7-1.63-1.27-7.38 5.74zM12 16l7.36-5.73L21 9l-9-7-9 7 1.63 1.27L12 16z"
 		}, {
 			name: "AGENDA",
 			link: "#!/agenda",
-			id: 'agenda'
+			id: "agenda",
+			svg:"M3 5v6h5L7 7l4 1V3H5c-1.1 0-2 .9-2 2zm5 8H3v6c0 1.1.9 2 2 2h6v-5l-4 1 1-4zm9 4l-4-1v5h6c1.1 0 2-.9 2-2v-6h-5l1 4zm2-14h-6v5l4-1-1 4h5V5c0-1.1-.9-2-2-2z"
 		}, {
 			name: "PHOTOS",
 			link: "#!/albums",
-			id: 'albums'
+			id: "albums",
+			svg:"M4 11h5V5H4v6zm0 7h5v-6H4v6zm6 0h5v-6h-5v6zm6 0h5v-6h-5v6zm-6-7h5V5h-5v6zm6-6v6h5V5h-5z"
 		}, {
 			name: "L'EQUIPE",
 			link: "#!/users",
-			id: 'users'
+			id: "users",
+			svg:"M22 18v-2H8V4h2L7 1 4 4h2v2H2v2h4v8c0 1.1.9 2 2 2h8v2h-2l3 3 3-3h-2v-2h4zM10 8h6v6h2V8c0-1.1-.9-2-2-2h-6v2z"
 		}, {
 			name: "AUTRES",
-			id: 'suggestions',
+			id: "suggestions",
+			link:"issues",
+			svg:"M16 6l2.29 2.29-4.88 4.88-4-4L2 16.59 3.41 18l6-6 4 4 6.3-6.29L22 12V6z",
 			children: [{
 				name: "Suggestions",
 				link: "#!/suggestions",
