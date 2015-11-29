48fed515032571327ecc5c5888eb265301cc3b57
alexrosepizant <alexandre.rosepizant@gmail.com>
2015-11-29 03:09:41 +0100

    Fixes

--- server/server.js
+++ server/server.js
@@ -81,9 +81,9 @@ logger.init( app, passport, mongoose );
 var cronjob = require('./cron.js');
 cronjob.startCron();
 
-// Start crons
-var script = require('./script.js');
+// Start scripts
+/*var script = require('./script.js');
 script.startScript();
-
+*/
 // Expose app
 exports = module.exports = app;
