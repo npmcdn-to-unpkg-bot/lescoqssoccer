1b4ca7f4a229a2ed0835375b2027edf3756da528
alexrosepizant <alexandre.rosepizant@gmail.com>
2015-11-29 01:29:53 +0100

    Fix suggestion

--- server/script.js
+++ server/script.js
@@ -6,16 +6,17 @@ var mongoose = require('mongoose'),
 	Suggestion = mongoose.model('Suggestion'),
 	_ = require('lodash');
 
+exports.startScript = function() {
+	Suggestion.find().exec(function(err, suggestions) {
+		var no, blank;
+			_.each(suggestions, function(suggestion){
+				no = suggestion.no;
+				suggestion.no = suggestion.blank;
+				suggestion.blank = no;
 
-Suggestion.find().exec(function(err, suggestions) {
-	var no, blank;
-		_.each(suggestions, function(suggestion){
-			no = suggestion.no;
-			suggestion.no = suggestion.blank;
-			suggestion.blank = no;
-
-			suggestion.save(function(err) {
-				console.warn("mis à jour")			
+				suggestion.save(function(err) {
+					console.warn("mis à jour")			
+				});
 			});
 		});
-	});
+};
